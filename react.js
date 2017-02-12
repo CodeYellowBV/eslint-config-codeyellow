db61546a8940a41625980f2b4a00ebfb28544254
Kees Kluskens <kees@webduck.nl>
2017-02-12 17:35:17 +0100

    Add babel-eslint as parser (because decorator support)

--- react.js
+++ react.js
@@ -6,6 +6,9 @@ module.exports = {
         'eslint-config-airbnb/rules/react-a11y',
         'eslint-config-codeyellow/rules/base',
     ].map(require.resolve),
+    // The only reason we need babel-eslint is because the default parser doesn't support decorators.
+    // As soon as the default parser supports this, please remove babel-eslint.
+    parser: 'babel-eslint',
     rules: {
         // Force to 4 spaces
         'react/jsx-indent-props': [2, 4],
